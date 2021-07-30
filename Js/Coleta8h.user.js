8dc90b9e23998c0cdce0ee7c375dae451cac42d9
PunnKeD <59070967+PunnKeD@users.noreply.github.com>
2021-07-30 17:06:28 -0300

    Add files via upload

--- Js/Coleta8h.user.js
+++ Js/Coleta8h.user.js
@@ -2,4 +2,4 @@
 // @name                Coletar Recursos 8h
 // @namespace           @@MeleeGoD
 // ==/UserScript==
-*/javascript:$.getScript('https://dl.dropboxusercontent.com/s/pt7qemvv9w6xagy/8h%20Coletor.js?dl=0');void(0);
+* javascript:$.getScript('https://dl.dropboxusercontent.com/s/pt7qemvv9w6xagy/8h%20Coletor.js?dl=0');void(0);
