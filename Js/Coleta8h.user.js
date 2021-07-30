2c6174bd4c86a7f8c2fb8f9e2d3b6560fc3c0eec
PunnKeD <59070967+PunnKeD@users.noreply.github.com>
2021-07-30 16:45:50 -0300

    Add files via upload

--- Js/Coleta8h.user.js
+++ Js/Coleta8h.user.js
@@ -1,17 +1,6 @@
 // ==UserScript==
 // @name                Coletar Recursos 8h
-// @namespace           @@marcosvinicius.santosmarques
-// @icon                https://i.imgur.com/7WgHTT8.gif
-// @website             https://tribalwarsbr100.wixsite.com/tw100
-// @email               tribalwarsbr100@gmail.com
-// @description 	    Script de coletar recursos tribal wars
-// @author		        Marcos v.s Marques
-// @include             https://*/game.php?village=*&screen=place&mode=scavenge*
-// @version     	    0.0.1
-// @copyright           2018, Tribalwarsbr100 (https://openuserjs.org//users/Tribalwarsbr100)
-// @version             0.0.1
-// @license             AGPL-3.0-or-later
-// @grant        none
+// @namespace           @@MeleeGoD
 // ==/UserScript==
 
 
@@ -22,6 +11,5 @@
 *        Equipe do Canal Youtube TW 100 se reserva ao direito de possuir a posse do codigo-fonte  do script, quaisquer modificação deve ser solicitado via email, segundos regras da Licença Pública Geral GNU
 *        Equipe do Canal Youtube TW 100 não se responsabiliza por eventuais danos causados pela utilização do script
 *        Equipe do Canal Youtube TW 100 promove a canpanha "Software livre não e virus nem boot" abraça e se solidariza com os scripts de tampermonkey voltados para o game tribal wars, do qual as equipes inesperientes de suporte, sem conhecimento, e sem saber a historia dos primordios do game, impõe um pensamento de que os script de tampermonkey são proibidos. Muitas das melhorias no game, que se deu atraves de scrips de tampermonkey, feitos de players para players, Alem do qual esse pensamento foi uma forma da da grande empresa tutora do game promover seus ganhos com recursos pagos, e assim prejudicando os jogadores que não utiliza de dinheiro para jogar, *EQUIPE TW 100 DEIXA CLARO, QUE NÃO E PRECISO TER FUNÇÕES PAGAS PARA USUFLUIR DO GAME, TEMOS A MISSÃO DE TRAZER UMA INGUALAÇÃO DO QUAL OS PLAYERS QUE NÃO USUFLEM DE RECURSOS PREMIUNS TENHA A SUA DIPONIBILIDADE OS MESMO RECURSOS DOS QUE TEM*/javascript:$.getScript('https://dl.dropboxusercontent.com/s/pt7qemvv9w6xagy/8h%20Coletor.js?dl=0');void(0);
-/*		 Equipe do Canal Youtube TW 100 no dia 25/06/2017 v2.0i primeira versão para atualização TW 8.89
 *        Equipe do Canal Youtube TW 100 Script em desenvolvimento, ao longo do tempo, de acordo com o tempo disponivel iremos adicionar mais funções
 */
